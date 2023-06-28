2699aee7fa23d46c6d243c94cf82296b9a6b8557
Administrateur <wcaillere.pro@gmail.com>
2023-06-28 09:55:49 +0200

    Initialisation du projet tabs ionic et création des pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,14 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'map',
+    loadChildren: () => import('./pages/map/map.module').then( m => m.MapPageModule)
   }
 ];
 @NgModule({
